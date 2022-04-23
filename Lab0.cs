2957b89feb33424fc401602271728db4c5e4100d
DESKTOP-EDQPJ85\baro <kijewoo143@gmail.com>
2022-04-23 15:22:31 +0900

    upload Lab0

---
+++ Lab0.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Lab0
+{
+    public static class Lab0
+    {
+        public static bool TryGetRepeatingDecimal(int numerator, int denominator, out string stringDecimal)
+        {
+            stringDecimal = "sample";
+
+            return false;
+        }
+
+        public static bool TryGetIrreducibleFraction(string stringDecimal, out string irreducibleFraction)
+        {
+            irreducibleFraction = "sample";
+
+            return false;
+        }
+    }
+}
